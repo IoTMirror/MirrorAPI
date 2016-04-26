60ae9b2744ae3a0f16366a0c4b8d64e19a2553d7
Jacek Przemieniecki <github@przemieniecki.net>
2016-04-26 20:39:14 +0200

    moved to new repo

---
+++ main.py
@@ -0,0 +1,208 @@
+import os
+import uuid
+import requests
+
+from functools import wraps
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import PrimaryKeyConstraint
+
+home_dir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
+app.debug = True
+db = SQLAlchemy(app)
+
+twitter_url = os.environ["TWITTER_URL"]
+
+
+class IdUserBinding(db.Model):
+    __tablename__ = 'device_user'
+    __table_args__ = (PrimaryKeyConstraint('device_id', 'user_id'),)
+    device_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+
+    def __init__(self, user_id, device_id):
+        self.user_id = user_id
+        self.device_id = device_id
+
+
+class Session(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String(32))
+
+    def __init__(self, user_id, token):
+        self.id = user_id
+        self.token = token
+
+
+class UserConfig(db.Model):
+    __tablename__ = 'Config'
+    id = db.Column(db.Integer, primary_key=True)
+    twitter_x = db.Column(db.Integer)
+    twitter_y = db.Column(db.Integer)
+    twitter_w = db.Column(db.Integer)
+    twitter_h = db.Column(db.Integer)
+
+    photo_x = db.Column(db.Integer)
+    photo_y = db.Column(db.Integer)
+    photo_w = db.Column(db.Integer)
+    photo_h = db.Column(db.Integer)
+
+
+def load_session(token):
+    return Session.query.filter_by(token=token).first()
+
+
+def send_new_session_notification(device_id):
+    request_body = jsonify({"mirrorID": device_id})
+    return 12345
+
+
+def confirm_face_recognition(token, recognition_token):
+    request_body = jsonify({'token': token, 'recognition_token': recognition_token})
+    return 1
+
+
+recognition_service_url = "http://localhost"
+
+
+@app.route("/login/start_session", methods=['POST'])
+def login_start_session():
+    body = request.get_json()
+    if 'DeviceId' not in body:
+        return jsonify({'error': 'DeviceId missing'}), 401
+
+    device_id = body['DeviceId']
+    users_bound = [e.user_id for e in IdUserBinding.query.filter_by(device_id=device_id)]
+    session_token = send_new_session_notification(device_id)
+
+    return jsonify({'LoginToken': session_token}), 200
+
+
+@app.route("/login/confirm", methods=['POST'])
+def login_confirm_session():
+    body = request.get_json()
+    if 'LoginToken' not in body:
+        return jsonify({'error': 'Token missing'}), 401
+    if 'RecognitionToken' not in body:
+        return jsonify({'error': 'RecognitionToken missing'}), 401
+
+    token = body['LoginToken']
+    recognition_token = body['RecognitionToken']
+
+    user_id = confirm_face_recognition(token, recognition_token)
+    if user_id is -1:
+        return jsonify({'error': 'Invalid token'}), 401
+
+    login_session_token = uuid.uuid4().hex
+    ses = Session.query.get(user_id)
+    if ses:
+        ses.token = login_session_token
+    else:
+        ses = Session(user_id, login_session_token)
+    db.session.add(ses)
+    db.session.commit()
+    config = UserConfig.session.get(user_id)
+
+    widgets = []
+    if config.twitter_w > 0:
+        widgets.append({
+            "WidgetName": "Twitter",
+            "WidgetType": "Small",
+            "WidgetPosition": {
+                "X": config.twitter_x,
+                "Y": config.twitter_y
+            },
+            "WidgetSize": {
+                "X": config.twitter_w,
+                "Y": config.twitter_h
+            }
+        })
+    if config.photo_w > 0:
+        widgets.append({
+            "WidgetName": "Photo",
+            "WidgetType": "Small",
+            "WidgetPosition": {
+                "X": config.photo_x,
+                "Y": config.photo_y
+            },
+            "WidgetSize": {
+                "X": config.photo_w,
+                "Y": config.photo_h
+            }
+        })
+    return jsonify({'Token': login_session_token, 'Widgets': widgets})
+
+
+def requires_login_get(f):
+    @wraps(f)
+    def inner():
+        if "token" not in request.args:
+            return jsonify({"error": "token missing"})
+        token = request.args["token"]
+        session = load_session(token)
+        if not session:
+            return jsonify({"error": "Not logged in"})
+        return f(session.id)
+    return inner
+
+
+def requires_login_post(f):
+    @wraps(f)
+    def inner():
+        body = request.get_json()
+        if "token" not in body:
+            return jsonify({"error": "token missing"}), 400
+        session = load_session(body["token"])
+        if not session:
+            return jsonify({"error": "Not logged in"})
+        return f(session.id)
+    return inner
+
+
+@app.route("/logout", methods=['POST'])
+def logout():
+    body = request.get_json()
+    token = body['Token']
+    session = load_session(token)
+    if not session:
+        return jsonify({'Success': 'False', 'Reason': 'Not logged in'})
+    db.session.delete(session)
+    db.session.commit()
+
+    return jsonify({'Success': "true"})
+
+
+@app.route("/test", methods=['POST'])
+@requires_login_post
+def test(user_id):
+    return jsonify({'Status': "Logged in"})
+
+
+@app.route("/facebook", methods=['GET'])
+@requires_login_get
+def facebook(user_id):
+    return '{"data": "placeholder"}'
+
+
+@app.route("/twitter", methods=['GET'])
+@requires_login_get
+def twitter(user_id):
+    url = "{0}users/{1}/home_timeline".format(twitter_url, user_id)
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        return resp.content
+    else:
+        return jsonify({"error": "User not logged into twitter"})
+
+
+@app.route("/gmail", methods=['GET'])
+@requires_login_get
+def gmail(user_id):
+    return '{"data": "placeholder"}'
+
+
+if __name__ == "__main__":
+    app.run()
